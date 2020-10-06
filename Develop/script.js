97d96c6a1a6d18fb8f35265bf38295272a728650
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-10-05 23:32:58 -0400

    Styled page css, outlined html elements, began creating javascript variables and elements

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,16 @@
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and score
+// DOM Elements (depencies)
+
+var site1El = document.querySelector(".site1");
+var myButton = document.createElement("button");
+
+// Data
+site1El.children[0].textContent = "Are you ready to test your knowledge?";
+site1El.setAttribute("style", "text-align: center; font-family: 'VT323', monospace; font-size: 4rem;");
+myButton.textContent = "I'm a button";
+myButton.setAttribute("style", "font-family: 'VT323', monospace; font-size: 2rem; border: white solid 1px; background-color: black; color: white");
+myButton.addEventListener("click", codeQuiz);
+site1El.appendChild(myButton);
+
+function codeQuiz() {
+    myButton.style.display = "none";
+}
