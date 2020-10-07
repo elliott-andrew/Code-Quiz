926416a6f5929cb052dc3a4b250d130c53088615
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-10-07 19:51:23 -0400

    Created function to pull current question index number

--- Develop/script.js
+++ Develop/script.js
@@ -15,7 +15,7 @@ var questions = [
         answer1: "Hypertext Markdown Language",
         answer2: "Hypertext Markup Language",
         answer3: "Hypertext Marky Mark Language",
-        correct: "Hypertext Markup Language"
+        correct: 2
     },
 
     {
@@ -23,7 +23,7 @@ var questions = [
         answer1: "Cascading Stylesheet",
         answer2: "Flowing Stylesheet",
         answer3: "Shading Stylesheet",
-        correct: "Cascading Stylesheet"
+        correct: 1
     },
 
     {
@@ -31,7 +31,7 @@ var questions = [
         answer1: "A typeface",
         answer2: "A coffee brand",
         answer3: "A programming language",
-        correct: "A programming language"
+        correct: 3
     },
 
     {
@@ -39,8 +39,61 @@ var questions = [
         answer1: "Document Object Model",
         answer2: "Document Thingamajig",
         answer3: "Donut Object Mmmmmm",
-        correct: "Document Object Model",
+        correct: 1
     }
 
 ]
-console.log(questions[0].question);
+console.log(questions.length);
+
+var numQuestions = questions.length;
+var questionIndex = 0;
+
+// Helper functions
+startButton.addEventListener("click", function () {
+    startButton.style.display = "none";
+    renderQuestions();
+});
+
+function renderQuestions() {
+    var currentQuestionIndex = Math.floor(questionIndex * numQuestions);
+    yourQuestion = questions[currentQuestionIndex]
+    questionDisplay.textContent = yourQuestion.question
+
+}
+
+
+// function questionOne() {
+//     questionDisplay.innerHTML = "";
+//     var h2 = document.createElement("h2");
+//     h2.textContent = questions[0].question;
+//     h2.classList.add("question")
+//     questionDisplay.appendChild(h2);
+
+// }
+
+// function questionTwo() {
+//     questionDisplay.innerHTML = "";
+//     var h2 = document.createElement("h2");
+//     h2.textContent = questions[1].question;
+//     h2.classList.add("question")
+//     questionDisplay.appendChild(h2);
+
+// }
+
+// function questionThree() {
+//     questionDisplay.innerHTML = "";
+//     var h2 = document.createElement("h2");
+//     h2.textContent = questions[2].question;
+//     h2.classList.add("question")
+//     questionDisplay.appendChild(h2);
+
+// }
+
+// function questionFour() {
+//     questionDisplay.innerHTML = "";
+//     var h2 = document.createElement("h2");
+//     h2.textContent = questions[3].question;
+//     h2.classList.add("question")
+//     questionDisplay.appendChild(h2);
+
+// }
